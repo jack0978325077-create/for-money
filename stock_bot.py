b36f451ed5feebd651b7a4345ab53c8c94b64012
jack0978325077-create <jack0978325077@gmail.com>
2026-08-07 08:30:36 +0000

    Bind Flask to 0.0.0.0 for Render port detection

--- stock_bot.py
+++ stock_bot.py
@@ -79,5 +79,8 @@ def callback():
             
     return 'OK'
 
-if __name__ == "__main__":
-    app.run(port=5000)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
